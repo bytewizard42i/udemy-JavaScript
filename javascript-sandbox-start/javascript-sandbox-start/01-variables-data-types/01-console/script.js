39eadca593b7419cecad5db93bd1efb4caed253f
johnny5cfw <john@cryptofinancialworld.com>
2024-05-29 23:39:11 -0400

    awesome learning session. this dude really knows how to teach.

--- javascript-sandbox-start/javascript-sandbox-start/01-variables-data-types/01-console/script.js
+++ javascript-sandbox-start/javascript-sandbox-start/01-variables-data-types/01-console/script.js
@@ -5,9 +5,8 @@ console.log('\n');
 console.log('Hello World!');
 
 console.log('\n');
-
-
 console.log(20, 'hello', true);   
+
 
 console.log('\n');
 
@@ -16,5 +15,19 @@ console.log("x =", x);
 
 /* 
 snippet shortcut: clg[enter]
- */
+*/
+console.error('This is an error message!');
+
+console.warn('This is a warning message!');
+
+console.table({a: 1, b: 2});
+
+console.group('Johns simple collapsable group');
+console.log('x -', x);
+console.error('Alert, John!!!');
+console.warn('Warning, John!!!');
+console.groupEnd();
+
+const styles = 'color: blue; font-size: 20px; background-color: yellow; padding: 10px;';
+console.log('%cHello My stylish World!', styles);
 
