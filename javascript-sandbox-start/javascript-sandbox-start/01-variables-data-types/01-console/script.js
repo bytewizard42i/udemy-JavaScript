e959e2af6210768c850542b5f0c03a8f1a7aef55
johnny5cfw <john@cryptofinancialworld.com>
2024-05-29 22:15:47 -0400

    Continuing my js education. This teacher is amazing. So many little treasure nuggets of information with all context.

--- javascript-sandbox-start/javascript-sandbox-start/01-variables-data-types/01-console/script.js
+++ javascript-sandbox-start/javascript-sandbox-start/01-variables-data-types/01-console/script.js
@@ -1,8 +1,20 @@
 console.log(100);
 
+console.log('\n');
+
 console.log('Hello World!');
 
-console.log(20, 'hello', true);      
+console.log('\n');
+
+
+console.log(20, 'hello', true);   
+
+console.log('\n');
 
 const x = 100;
-console.log(x);
+console.log("x =", x);
+
+/* 
+snippet shortcut: clg[enter]
+ */
+
