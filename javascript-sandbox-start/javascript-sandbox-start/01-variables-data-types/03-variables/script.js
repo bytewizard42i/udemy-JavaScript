dc28fbc32487a4e2b7a745236618e4b161725395
johnny5cfw <john@cryptofinancialworld.com>
2024-06-02 00:20:18 -0400

    cleaning up my code

--- javascript-sandbox-start/javascript-sandbox-start/01-variables-data-types/03-variables/script.js
+++ javascript-sandbox-start/javascript-sandbox-start/01-variables-data-types/03-variables/script.js
@@ -4,12 +4,17 @@
 let firstName = 'John';
 let lastName = 'Doe';
 
+// Generally speaking always use const unless you know you are going to reassign the value of the variable
+
 console.log(firstName, lastName);
+
+console.log('\n');
 
 let age = 30;
 
 console.log(age);
 
+console.log('\n');
 
 // Naming Conventions
 // - Only letters, numbers, underscores and dollar signs
@@ -24,8 +29,15 @@ console.log(age);
 
 // reassigning variables
 
+age = 35;
+console.log('Your new age is', age);
+
+console.log('\n');
+
 firstName = 'Jeff';
-console.log(firstName); 
+console.log('Your first name is',firstName); 
+
+console.log('\n');
 
 
 
