459f9399214436590a6d512de0295d64160ca41b
bytewizard42i <100811071+bytewizard42i@users.noreply.github.com>
2024-06-07 21:42:18 -0400

    Yeay! partially completed

---
+++ javascript-sandbox-start/javascript-sandbox-start/01-variables-data-types/13-number-challenge/script.js
@@ -0,0 +1,13 @@
+//* Number Challenge
+
+let x = Math.floor(Math.random() * 100) + 1;
+
+let y = Math.floor(Math.random() * 50) + 1;
+
+let randomSum = x + y;
+
+console.log('\n');
+
+console.log('The solution to the number challenge is: Random number x from 1-100 =', x, '+', 'Random number from 1-50 =', y, '=', randomSum);    
+
+console.log('\n');
