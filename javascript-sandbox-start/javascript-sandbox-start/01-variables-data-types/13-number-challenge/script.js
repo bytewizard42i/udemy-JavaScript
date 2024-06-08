d46a785ad6d266547a5977dfce7aa9edb2451be3
bytewizard42i <100811071+bytewizard42i@users.noreply.github.com>
2024-06-07 21:51:04 -0400

    Done with number challenge! I think I did great. Yeay!!!

--- javascript-sandbox-start/javascript-sandbox-start/01-variables-data-types/13-number-challenge/script.js
+++ javascript-sandbox-start/javascript-sandbox-start/01-variables-data-types/13-number-challenge/script.js
@@ -4,10 +4,78 @@ let x = Math.floor(Math.random() * 100) + 1;
 
 let y = Math.floor(Math.random() * 50) + 1;
 
-let randomSum = x + y;
-
 console.log('\n');
+
+//*---------------------------------*/
+
+let randomSum = x + y;
 
 console.log('The solution to the number challenge is: Random number x from 1-100 =', x, '+', 'Random number from 1-50 =', y, '=', randomSum);    
 
 console.log('\n');
+
+//*---------------------------------*/
+
+let randomDifference = x - y;
+
+console.log('The solution to the number challenge is: Random number x from 1-100 =', x, '-', 'Random number from 1-50 =', y, '=', randomDifference);
+
+console.log('\n');
+
+//*---------------------------------*/
+
+let randomProduct = x * y;
+
+console.log('The solution to the number challenge is: Random number x from 1-100 =', x, '*', 'Random number from 1-50 =', y, '=', randomProduct);
+
+console.log('\n');
+
+//*---------------------------------*/
+
+let randomQuotient = x / y;
+
+console.log('The solution to the number challenge is: Random number x from 1-100 =', x, '/', 'Random number from 1-50 =', y, '=', randomQuotient);
+
+console.log('\n');
+
+//*---------------------------------*/
+
+let randomModulus = x % y;
+
+console.log('The solution to the number challenge is: Random number x from 1-100 =', x, '%', 'Random number from 1-50 =', y, '=', randomModulus);
+
+console.log('\n');
+
+//*---------------------------------*/
+
+let randomExponent = x ** y;
+
+console.log('The solution to the number challenge is: Random number x from 1-100 =', x, '**', 'Random number from 1-50 =', y, '=', randomExponent);
+
+console.log('\n');
+
+//*---------------------------------*/
+
+let randomIncrement = x++;
+
+console.log('The solution to the number challenge is: Random number x from 1-100 =', x, '++', '=', randomIncrement);
+
+console.log('\n');
+
+//*---------------------------------*/
+
+let randomDecrement = x--;
+
+console.log('The solution to the number challenge is: Random number x from 1-100 =', x, '--', '=', randomDecrement);
+
+console.log('\n');
+
+//*---------------------------------*/
+
+let randomNegation = -x;
+
+console.log('The solution to the number challenge is: Random number x from 1-100 =', '-', x, '=', randomNegation);
+
+console.log('\n');
+
+//*---------------------------------*/
