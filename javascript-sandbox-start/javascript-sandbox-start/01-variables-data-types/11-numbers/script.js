5c5415cdc04153e0f40e3088f1b529bdbb149845
bytewizard42i <100811071+bytewizard42i@users.noreply.github.com>
2024-06-07 20:39:35 -0400

    This is getting fun. Continueing to learn about manipulating numbers. I didn't realize there was so much to learn in this section, but it is quite crucial.

--- javascript-sandbox-start/javascript-sandbox-start/01-variables-data-types/11-numbers/script.js
+++ javascript-sandbox-start/javascript-sandbox-start/01-variables-data-types/11-numbers/script.js
@@ -1,8 +1,124 @@
-const num = 5;
+console.log(Math);
 
 console.log('\n');
+
+//*--------------------------------------------*
+
+const num = 5;
 
 console.log('The type of the variable num is:', typeof num); 
 
 console.log('\n');
+
+//*--------------------------------------------*
+
+let x;
+
+x = Math.sqrt( 9);
+
+console.log('square root of 9 is:', x);
+
+console.log('\n');
+
+//*--------------------------------------------*
+
+x = Math.abs(-5);
+
+console.log('absolute value of -5 is:', x);
+
+console.log('\n');
+
+x = Math.abs(5);
+
+console.log('absolute value of 5 is:', x);
+
+console.log('\n');
+
+//*--------------------------------------------*
+
+x = Math.round(4.2);
+
+console.log('round 4.2 =', x);
+
+console.log('\n');
+
+//*--------------------------------------------*
+
+x = Math.ceil(4.2);
+
+console.log('ceil 4.2', x);
+
+console.log('\n');
+
+//*--------------------------------------------*
+
+x = Math.floor(4.2);
+
+console.log('floor 4.2', x);
+
+console.log('\n');
+
+//*--------------------------------------------*
+
+x = Math.pow(2, 3);
+
+console.log('pow 2, 3', x);
+
+console.log('\n');
+
+//*--------------------------------------------*
+
+x = Math.min(4, 5, 6, 7, 8, 9);
+
+console.log('min 4, 5, 6, 7, 8, 9', x);
+
+console.log('\n');
+
+//*--------------------------------------------*
+
+x = Math.max(4, 5, 6, 7, 8, 9);
+
+console.log('max 4, 5, 6, 7, 8, 9', x);
+
+console.log('\n');
+
+//*--------------------------------------------*`
+
+x = Math.random();
+
+console.log('random number =', x);
+
+console.log('\n');
+
+//*--------------------------------------------*
+
+x = Math.random() * 10;
+
+console.log('random number * 10 =', x);
+
+console.log('\n');
+
+//*--------------------------------------------*
+
+x = Math.floor(Math.random() * 10);
+
+console.log('random number * 10 =', x);
+
+console.log('\n');
+
+//*--------------------------------------------*
+
+x = Math.floor(Math.random() * 10) + 1;
+
+console.log('random number * 10 + 1 =', x);
+
+console.log('\n');
+
+//*--------------------------------------------*
+
+
+
+
+
+
 
