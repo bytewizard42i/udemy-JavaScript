672da230f0a05b66c060352f3689aad529afbe12
bytewizard42i <100811071+bytewizard42i@users.noreply.github.com>
2024-06-08 14:47:06 -0400

    working with dates and times

---
+++ javascript-sandbox-start/javascript-sandbox-start/01-variables-data-types/14-dates-and-times/script.js
@@ -0,0 +1,19 @@
+let d;
+
+d = new Date();
+
+console.log('\n');
+
+// d = d.toString();
+
+console.log(d,typeof d);
+
+// d = new Date('6-07-2024');
+
+/*
+d = new Date(2021, 0, 10);
+
+console.log(d);
+
+console.log('\n');
+*/
