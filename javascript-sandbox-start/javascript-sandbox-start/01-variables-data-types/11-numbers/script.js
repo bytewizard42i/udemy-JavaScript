357aec5d63f9fac65c4e7b1c96d240ccf8a5f70b
bytewizard42i <100811071+bytewizard42i@users.noreply.github.com>
2024-06-07 21:11:24 -0400

    great lesson

--- javascript-sandbox-start/javascript-sandbox-start/01-variables-data-types/11-numbers/script.js
+++ javascript-sandbox-start/javascript-sandbox-start/01-variables-data-types/11-numbers/script.js
@@ -44,9 +44,17 @@ console.log('\n');
 
 //*--------------------------------------------*
 
+x = Math.round(4.5);
+
+console.log('round 4.5 =', x);
+
+console.log('\n');
+
+//*--------------------------------------------*
+
 x = Math.ceil(4.2);
 
-console.log('ceil 4.2', x);
+console.log('ceil of 4.2 =', x);
 
 console.log('\n');
 
@@ -54,7 +62,7 @@ console.log('\n');
 
 x = Math.floor(4.2);
 
-console.log('floor 4.2', x);
+console.log('floor of 4.2 =', x);
 
 console.log('\n');
 
@@ -62,7 +70,7 @@ console.log('\n');
 
 x = Math.pow(2, 3);
 
-console.log('pow 2, 3', x);
+console.log('pow of 2, 3 =', x);
 
 console.log('\n');
 
@@ -70,7 +78,7 @@ console.log('\n');
 
 x = Math.min(4, 5, 6, 7, 8, 9);
 
-console.log('min 4, 5, 6, 7, 8, 9', x);
+console.log('min 4, 5, 6, 7, 8, 9 =', x);
 
 console.log('\n');
 
@@ -78,7 +86,7 @@ console.log('\n');
 
 x = Math.max(4, 5, 6, 7, 8, 9);
 
-console.log('max 4, 5, 6, 7, 8, 9', x);
+console.log('max 4, 5, 6, 7, 8, 9 =', x);
 
 console.log('\n');
 
@@ -102,7 +110,7 @@ console.log('\n');
 
 x = Math.floor(Math.random() * 10);
 
-console.log('random number * 10 =', x);
+console.log('floor(random number * 10) =', x);
 
 console.log('\n');
 
@@ -110,12 +118,19 @@ console.log('\n');
 
 x = Math.floor(Math.random() * 10) + 1;
 
-console.log('random number * 10 + 1 =', x);
+console.log('floor(random number * 10) + 1 =', x);
 
 console.log('\n');
 
 //*--------------------------------------------*
 
+x = Math.floor(Math.random() * 100) + 1;
+
+console.log('floor(random number * 100) + 1 =', x);
+
+console.log('\n');
+
+//*--------------------------------------------*
 
 
 
