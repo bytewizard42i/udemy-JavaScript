d9482815be0c651c76e52b475b1767ae6f3e6486
bytewizard42i <100811071+bytewizard42i@users.noreply.github.com>
2024-06-09 11:24:09 -0400

    fun with arrays

--- javascript-sandbox-start/javascript-sandbox-start/02-arrays-and-objects/01-array-basics/script.js
+++ javascript-sandbox-start/javascript-sandbox-start/02-arrays-and-objects/01-array-basics/script.js
@@ -1,12 +1,18 @@
 
 console.log('\n');
 
-// array literal
-
+//* array literal
 const numbers = [1, 2, 3, 4, 5];
 
-// array constructor
+console.log(typeof numbers, numbers);
 
+console.log('\n');
+
+console.log('The number in the 4th position \nof the array starting with 0 is:', numbers[4]);
+
+console.log('\n');
+
+//* array constructor
 const fruits = new Array('strawberry', 'banana', 'apple', 'orange', 'grape');
 
 console.log(fruits); 
