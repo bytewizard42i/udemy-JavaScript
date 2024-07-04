4f01605f190dd76ef9fe6aeed647580a04c608aa
bytewizard42i <100811071+bytewizard42i@users.noreply.github.com>
2024-07-04 16:44:50 -0400

    Learning abour arrow functions and shortening my code. Getting comfortable with the VSCode interface as well as the browser and inspect tool.

---
+++ javascript-sandbox-start/javascript-sandbox-start/03-functions-scope/06-declaration-vs-expression/script.js
@@ -0,0 +1,27 @@
+console.log('\n');
+
+console.log('-----------Begin module--------------');
+
+console.log('\n');
+
+// Function Declaration
+console.log('Function declaration');
+function addDollarSign(value) {
+    return '$' + value;
+}
+
+console.log(addDollarSign(100));
+
+console.log('\n');
+
+console.log('-----------------------------------');
+// Function Expression
+console.log('Function expression');
+const addPlusSign = function (value) {
+    return '+' + value;
+}
+console.log(addPlusSign(200));
+
+console.log('\n');
+
+
---
+++ javascript-sandbox-start/javascript-sandbox-start/03-functions-scope/07-arrow-functions/script.js
@@ -0,0 +1,42 @@
+
+// Arrow Functions
+// example 1
+
+// console.log('\n');
+// console.log('***Beginning of module***');
+// console.log('\n');
+ 
+// function add(a,b) {
+//     return a + b
+// }
+console.log('\n');
+
+// normal Arrow f(x) syntax -another way to make the function
+// example 2
+
+const add = (a, b) => {
+    return a + b;
+};
+
+console.log(add(1, 2));
+console.log('\n');
+
+// We can make this Arrow Syntax even shorter
+// We can get rid of the curly braces and return statement
+
+const subtract = (a, b) => a - b;
+
+console.log(subtract(10, 2));
+console.log('\n');
+
+
+
+
+console.log('***End of module***');
+console.log('\n');
+
+
+
+
+
+
