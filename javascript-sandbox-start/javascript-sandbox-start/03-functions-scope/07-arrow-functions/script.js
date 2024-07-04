8d4477344499ed0a1570c182dbfa2058b5439844
bytewizard42i <100811071+bytewizard42i@users.noreply.github.com>
2024-07-04 17:01:59 -0400

    adding more material about shortening up arrow functions. I need to learn how to present information as a template literal with proper syntax, having trouble.

--- javascript-sandbox-start/javascript-sandbox-start/03-functions-scope/07-arrow-functions/script.js
+++ javascript-sandbox-start/javascript-sandbox-start/03-functions-scope/07-arrow-functions/script.js
@@ -24,12 +24,17 @@ console.log('\n');
 // We can make this Arrow Syntax even shorter
 // We can get rid of the curly braces and return statement
 
+// Implicit Return
 const subtract = (a, b) => a - b;
 
 console.log(subtract(10, 2));
 console.log('\n');
 
+// Even shorter, when just 1 param, dont need parentheses
+const double = a => a * 2;
 
+console.log(double(10));
+console.log('\n');
 
 
 console.log('***End of module***');
