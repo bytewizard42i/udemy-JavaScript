b00877c4716bbb5f04fb89a4eb2375579acfb043
bytewizard42i <100811071+bytewizard42i@users.noreply.github.com>
2024-07-25 22:36:09 -0400

    single line function attempt

--- javascript-sandbox-start/javascript-sandbox-start/03-functions-scope/09-function-challenges/script.js
+++ javascript-sandbox-start/javascript-sandbox-start/03-functions-scope/09-function-challenges/script.js
@@ -1,18 +1,24 @@
 
 //***function challenge 3.9
-console.log('\n');
-console.log('***Function Challenge 3.9***');
-console.log('\n');
+// console.log('\n');
+// console.log('***Function Challenge 3.9***');
+// console.log('\n');
 
 
-function getCelsius(tempF){
-    const fToC = (tempF - 32) * 5 / 9;
-    console.log(`The entered Fahrenheit temperature of ${tempF}\xB0F => Celsius = ${fToC}\xB0C `);
-    console.log('\n');
-}
+// function getCelsius(tempF){
+//     const fToC = (tempF - 32) * 5 / 9;
+//     console.log(`The entered Fahrenheit temperature of ${tempF}\xB0F => Celsius = ${fToC}\xB0C `);
+//     console.log('\n');
+// }
+
+//***Single line Function
+console.log('\n');
+console.log('single line function');
+const getCelsius = (tempF) => {
+    const fToC = ((tempF - 32) * 5 / 9);
+     console.log(`The entered Fahrenheit temperature of ${tempF}\xB0F => Celsius = ${fToC}\xB0C `)
+};
 
 getCelsius(32);
 
-
-
-
+console.log('\n');
