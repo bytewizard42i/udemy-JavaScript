c6d426650f59db47a9e7c67591d656d77939cef5
bytewizard42i <100811071+bytewizard42i@users.noreply.github.com>
2024-07-25 20:07:48 -0400

    part 1 of the cahllenge for f => C

--- javascript-sandbox-start/javascript-sandbox-start/03-functions-scope/09-function-challenges/script.js
+++ javascript-sandbox-start/javascript-sandbox-start/03-functions-scope/09-function-challenges/script.js
@@ -4,3 +4,15 @@ console.log('\n');
 console.log('***Function Challenge 3.9***');
 console.log('\n');
 
+
+function getCelsius(tempF){
+    const fToC = (tempF - 32) * 5 / 9;
+    console.log(`The entered Fahrenheit temperature of ${tempF}\xB0F => Celsius = ${fToC}\xB0C `);
+    console.log('\n');
+}
+
+getCelsius(32);
+
+
+
+
