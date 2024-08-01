e61e2c6134ff8799b5bcce7cf8e8e16ea9a8ddd9
bytewizard42i <100811071+bytewizard42i@users.noreply.github.com>
2024-08-01 03:28:55 -0400

    Took a little review, had trouble remembering to add the function and then the switch is subordinate to the funtion. Came out cute I think. This is pretty advanced. Im curious why they havent taught me to call for inputs instead of invoking the functions. This would be closer to real world use applications. e.g. "Please enter two numbers and an operator..."

--- javascript-sandbox-start/javascript-sandbox-start/04-logic-control-flow/04-calculator-challenge/script.js
+++ javascript-sandbox-start/javascript-sandbox-start/04-logic-control-flow/04-calculator-challenge/script.js
@@ -6,28 +6,56 @@ console.log('*** Beginning of Calculator Challenge 4.4 ***');
 console.log('\n');
 
 //*** if else calculator */
-console.log('---if else example---');
+// console.log('---if else example---');
+// console.log('\n');
+
+// function calculator(num1, num2, operator){
+//     if (operator === '*'){
+//         console.log(`Your entry of: ${num1} * ${num2} = ${num1 * num2}`);
+//     }
+
+//     else if (operator === '/'){
+//         console.log(`Your entry of: ${num1} / ${num2} = ${num1 / num2}`);
+//     }
+
+//     else if (operator === '+'){
+//         console.log(`Your entry of: ${num1} + ${num2} = ${num1 + num2}`);
+//     }
+
+//     else if (operator === '-'){
+//         console.log(`Your entry of: ${num1} - ${num2} = ${num1 - num2}`);
+//     }
+
+//     else console.log(`${operator} is not a valid operator for this exercise, please guess another operator!`);    
+// }
+
+//*** switch calculator */
+console.log('--- switch example---');
 console.log('\n');
 
 function calculator(num1, num2, operator){
-    if (operator === '*'){
-        console.log(`Your entry of: ${num1} * ${num2} = ${num1 * num2}`);
+  
+    switch (num1, num2, operator) {
+        case '*':
+            console.log(`The selection you made of ${num1} * ${num2} = ${num1 * num2}`);
+            break;
+        case '/':
+            console.log(`The selection you made of ${num1} / ${num2} = ${num1 / num2}`);
+            break;
+        case '+':
+            console.log(`The selection you made of ${num1} + ${num2} = ${num1 + num2}`);
+            break;
+        case '-':
+            console.log(`The selection you made of ${num1} - ${num2} = ${num1 - num2}`);
+            break;
+        default:
+            console.log(`The operator '${operator}' that you chose is not valid for this exercise, please guess again!`);
+            break;
     }
-
-    else if (operator === '/'){
-        console.log(`Your entry of: ${num1} / ${num2} = ${num1 / num2}`);
-    }
-
-    else if (operator === '+'){
-        console.log(`Your entry of: ${num1} + ${num2} = ${num1 + num2}`);
-    }
-
-    else if (operator === '-'){
-        console.log(`Your entry of: ${num1} - ${num2} = ${num1 - num2}`);
-    }
-
-    else console.log(`${operator} is not a valid operator for this exercise, please guess another operator!`);    
 }
+
+
+//*** f(x) invocations */
 
 calculator(2, 4, '*');
 console.log('\n');
