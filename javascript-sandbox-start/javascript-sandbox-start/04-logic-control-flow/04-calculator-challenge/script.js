998a50861a544021476c21c08bf330be0c115364
bytewizard42i <100811071+bytewizard42i@users.noreply.github.com>
2024-08-01 01:49:20 -0400

    beginning reference structure

--- javascript-sandbox-start/javascript-sandbox-start/04-logic-control-flow/04-calculator-challenge/script.js
+++ javascript-sandbox-start/javascript-sandbox-start/04-logic-control-flow/04-calculator-challenge/script.js
@@ -1,5 +1,11 @@
 
 //*** Calculator challenge 4.4 */
 console.log('\n');
+console.log('*******Beginning of Calculator Challenge 4.4 *******');
+console.log('\n');
 
+
+
+console.log('\n');
+console.log('******* End of Challenge 4.4 *******');
 console.log('\n');
