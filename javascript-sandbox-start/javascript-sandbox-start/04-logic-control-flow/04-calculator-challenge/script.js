62cec2f1d8a7c42b8a222a10622123863e2c1e11
bytewizard42i <100811071+bytewizard42i@users.noreply.github.com>
2024-08-01 02:47:24 -0400

    This took a little review. Starting to select multiple instances for editing to save time byholding alt and clicking, and relearned how to select multiple instances to add text also with alt.

--- javascript-sandbox-start/javascript-sandbox-start/04-logic-control-flow/04-calculator-challenge/script.js
+++ javascript-sandbox-start/javascript-sandbox-start/04-logic-control-flow/04-calculator-challenge/script.js
@@ -1,11 +1,49 @@
 
 //*** Calculator challenge 4.4 */
-console.log('\n');
-console.log('*******Beginning of Calculator Challenge 4.4 *******');
-console.log('\n');
-
-
 
 console.log('\n');
-console.log('******* End of Challenge 4.4 *******');
+console.log('*** Beginning of Calculator Challenge 4.4 ***');
+console.log('\n');
+
+//*** if else calculator */
+console.log('---if else example---');
+console.log('\n');
+
+function calculator(num1, num2, operator){
+    if (operator === '*'){
+        console.log(`Your entry of: ${num1} * ${num2} = ${num1 * num2}`);
+    }
+
+    else if (operator === '/'){
+        console.log(`Your entry of: ${num1} / ${num2} = ${num1 / num2}`);
+    }
+
+    else if (operator === '+'){
+        console.log(`Your entry of: ${num1} + ${num2} = ${num1 + num2}`);
+    }
+
+    else if (operator === '-'){
+        console.log(`Your entry of: ${num1} - ${num2} = ${num1 - num2}`);
+    }
+
+    else console.log(`${operator} is not a valid operator for this exercise, please guess another operator!`);    
+}
+
+calculator(2, 4, '*');
+console.log('\n');
+
+calculator(2, 4, '/');
+console.log('\n');
+
+calculator(2, 4, '+');
+console.log('\n');
+
+calculator(2, 4, '-');
+console.log('\n');
+
+calculator(2, 4, '%');
+console.log('\n');
+
+console.log('\n');
+console.log('*** End of Calculator Challenge 4.4 ***');
 console.log('\n');
