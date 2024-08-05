ae63a9855ed3b75cf6080e0642918e6d277495a2
bytewizard42i <100811071+bytewizard42i@users.noreply.github.com>
2024-08-05 01:09:24 -0400

    start to for loops 5.1

--- javascript-sandbox-start/javascript-sandbox-start/05-iteration-array-methods/01-for-loop/script.js
+++ javascript-sandbox-start/javascript-sandbox-start/05-iteration-array-methods/01-for-loop/script.js
@@ -5,3 +5,10 @@
 // CONDITION EXPRESSION - Condition that the loop will continue to run as long as it is met or until the condition is false
 // INCREMENT EXPRESSION - Expression that will be executed after each iteration of the loop. Usually increments the variable
 // STATEMENT - Code that will be executed each time the loop is run. To execute a `block` of code, use the `{}` syntax
+
+console.log('\n');
+console.log('--- beginning of exercise ---');
+console.log('\n');
+for (let i = 0; i <= 10; i++){
+    console.log('Number ' + i);
+}
