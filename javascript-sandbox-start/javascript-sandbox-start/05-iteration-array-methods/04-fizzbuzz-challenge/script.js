9b35a36095833840711c72b7e3f1e4d13fe863f5
bytewizard42i <100811071+bytewizard42i@users.noreply.github.com>
2024-08-05 01:43:36 -0400

    having trouble with asking for closing parentheses

--- javascript-sandbox-start/javascript-sandbox-start/05-iteration-array-methods/04-fizzbuzz-challenge/script.js
+++ javascript-sandbox-start/javascript-sandbox-start/05-iteration-array-methods/04-fizzbuzz-challenge/script.js
@@ -1,5 +1,16 @@
 
 console.log('\n');
-console.log('*** FizzBuzz Challenge ****');
+console.log('*** FizzBuzz Challenge ***');
 console.log('\n');
 
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 = 0) {
+        console.log('Fizz');
+        break;
+    }
+    if (i % 5 = 0) {
+        console.log('Buzz');
+    }else {
+        console.log(i);
+    }
+}
