f7cd94d973bfcc696abb2336d8f2db7f9c8fac7e
bytewizard42i <100811071+bytewizard42i@users.noreply.github.com>
2024-08-05 02:18:20 -0400

    WHOOPS!!! geesh forgot to increment i, and forgot to console i. yikes, I just crashed windows systems all over the world by not testing my update and releasing on a Friday!!! lol

--- javascript-sandbox-start/javascript-sandbox-start/05-iteration-array-methods/04-fizzbuzz-challenge/script.js
+++ javascript-sandbox-start/javascript-sandbox-start/05-iteration-array-methods/04-fizzbuzz-challenge/script.js
@@ -1,7 +1,7 @@
 
-console.log('\n');
-console.log('*** FizzBuzz Challenge ***');
-console.log('\n');
+// console.log('\n');
+// console.log('*** FizzBuzz Challenge ***');
+// console.log('\n');
 
 
 // for (let i = 1; i <= 100; i++) {
@@ -29,7 +29,8 @@ while(i <= 100) {
         console.log(`Buzz = ${i}`);
     }else if (i % 3 === 0 && i % 5 === 0) {
         console.log(`FizzBuzz = ${i}`);
-    }
+    }else {console.log(i);
+    }i++;    
 }
 
 
