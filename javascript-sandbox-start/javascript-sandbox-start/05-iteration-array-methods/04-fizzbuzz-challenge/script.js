c2ba3b5109463937d4cd09b8377cae291e8cb757
bytewizard42i <100811071+bytewizard42i@users.noreply.github.com>
2024-08-05 02:07:55 -0400

    Did it! while loop with some fancy output bits, to still see the number associated with I, just because Im fancy !!!@#$%%%^Yeayyyy!!!

--- javascript-sandbox-start/javascript-sandbox-start/05-iteration-array-methods/04-fizzbuzz-challenge/script.js
+++ javascript-sandbox-start/javascript-sandbox-start/05-iteration-array-methods/04-fizzbuzz-challenge/script.js
@@ -3,15 +3,36 @@ console.log('\n');
 console.log('*** FizzBuzz Challenge ***');
 console.log('\n');
 
-for (let i = 1; i <= 100; i++) {
+
+// for (let i = 1; i <= 100; i++) {
     
+//     if (i % 3 === 0) {
+//         console.log('Fizz');
+//     }else if (i % 5 === 0) {
+//         console.log('Buzz');
+//     }else if (i % 3 === 0 && i % 5 === 0) {
+//         console.log('FizzBuzz');
+//     }else {
+//         console.log(i);
+//     }
+// }
+
+console.log('\n');
+console.log('*** trying the challenge with while loop ***');
+
+let i = 1;
+
+while(i <= 100) {
     if (i % 3 === 0) {
-        console.log('Fizz');
+        console.log(`Fizz = ${i}`);
     }else if (i % 5 === 0) {
-        console.log('Buzz');
+        console.log(`Buzz = ${i}`);
     }else if (i % 3 === 0 && i % 5 === 0) {
-        console.log('FizzBuzz');
-    }else {
-        console.log(i);
+        console.log(`FizzBuzz = ${i}`);
     }
 }
+
+
+
+console.log('*** End FizzBuzz Challenge ***');
+console.log('\n');
