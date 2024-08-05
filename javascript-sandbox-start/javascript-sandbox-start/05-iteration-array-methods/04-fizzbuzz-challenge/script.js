1ad043e5e0a78ce38487881da1485ad6399c57e4
bytewizard42i <100811071+bytewizard42i@users.noreply.github.com>
2024-08-05 01:54:32 -0400

    A little help from chat gpt to tell me that I was using an = instead of === for a comparitor. Think I've got it now. Really fun and rewarding challenge. Yeayyy

--- javascript-sandbox-start/javascript-sandbox-start/05-iteration-array-methods/04-fizzbuzz-challenge/script.js
+++ javascript-sandbox-start/javascript-sandbox-start/05-iteration-array-methods/04-fizzbuzz-challenge/script.js
@@ -4,12 +4,13 @@ console.log('*** FizzBuzz Challenge ***');
 console.log('\n');
 
 for (let i = 1; i <= 100; i++) {
-    if (i % 3 = 0) {
+    
+    if (i % 3 === 0) {
         console.log('Fizz');
-        break;
-    }
-    if (i % 5 = 0) {
+    }else if (i % 5 === 0) {
         console.log('Buzz');
+    }else if (i % 3 === 0 && i % 5 === 0) {
+        console.log('FizzBuzz');
     }else {
         console.log(i);
     }
