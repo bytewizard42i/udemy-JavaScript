c2a48b6662301498b8d8b1e25eaada6978241d84
bytewizard42i <100811071+bytewizard42i@users.noreply.github.com>
2024-08-05 03:01:20 -0400

    getting tired 230 am, going to listen to next section and call it a night.

--- javascript-sandbox-start/javascript-sandbox-start/05-iteration-array-methods/05-for-of-loop/script.js
+++ javascript-sandbox-start/javascript-sandbox-start/05-iteration-array-methods/05-for-of-loop/script.js
@@ -1,18 +1,38 @@
 
 console.log('\n');
-console.log('*** For of loop 5.5***');
+console.log('*** "For of" loop 5.5***');
 console.log('\n');
 
+//*** "For of" */
+//*** Loop through arrays */
+const items = ['book', 'table', 'chair', 'kite'];
+const users = [{name: 'Brad' },{name: 'Kate' },{name: 'Steve' },];
 
 
+// for (const item of items) {
+//     console.log(items);
+// }
 
+for (const user of users) {
+    console.log(user.name);
+}
 
+//*** Loop through strings */
+const str = 'Hello World';
 
+for (const letter of str) {
+    console.log(letter);
+}
 
+//*** Loop over Map 
 
+const map = new Map();
+map.set('name', 'John');
+map.ser('age'. '30');
 
-
-
+for (const  [key, value] of map) {
+    console.log(key, value);
+}
 
 
 
